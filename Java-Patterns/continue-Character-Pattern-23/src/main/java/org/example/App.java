a77e5b820e6a884035edd0bbae68105cee22da3b
VinaySingh-04 <vinaygauraniya12@gmail.com>
2026-09-06 15:22:33 +0530

    Add Continue Characters Pattern

---
+++ Java-Patterns/continue-Character-Pattern-23/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class App {
+    public static void main( String[] args ) {
+        char ch ='A';
+        int n = 7;
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(ch +" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
